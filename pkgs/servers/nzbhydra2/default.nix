70f3c42976cd67e635be8559eb8d0351341d43d8
Nanotwerp <Nanotwerp@gmail.com>
2024-01-02 07:58:12 -0500

    nzbhydra2: 4.7.6 -> 5.3.5

--- pkgs/servers/nzbhydra2/default.nix
+++ pkgs/servers/nzbhydra2/default.nix
@@ -1,29 +1,29 @@
-{
-  lib,
-  stdenv,
-  fetchzip,
-  makeWrapper,
-  jre,
-  python3,
-  unzip,
+{ lib
+, stdenv
+, fetchzip
+, makeWrapper
+, jre
+, python3
+, unzip
+,
 }:
 stdenv.mkDerivation rec {
   pname = "nzbhydra2";
-  version = "4.7.6";
+  version = "5.3.5";
 
   src = fetchzip {
-    url = "https://github.com/theotherp/${pname}/releases/download/v${version}/${pname}-${version}-linux.zip";
-    hash = "sha512-vc+VInEnh00bASxcEwSjJcsa0QJHmtRzSz30uW60wGmA24tlaJYSk42N5KpGFbkQkOkb2ijHmKGxPogSa4izRQ==";
+    url = "https://github.com/theotherp/${pname}/releases/download/v${version}/${pname}-${version}-amd64-linux.zip";
+    hash = "sha512-l4k4vqCFMdImluu4mslGnz/WuYA/0Wy+3UKfHSHEqznrfEpN+7NHNhQSVGjf09311Rm4fkVuTeWmGEuRyHwS3w==";
     stripRoot = false;
   };
 
-  nativeBuildInputs = [jre makeWrapper unzip];
+  nativeBuildInputs = [ jre makeWrapper unzip ];
 
   installPhase = ''
     runHook preInstall
 
     install -d -m 755 "$out/lib/${pname}"
-    cp -dpr --no-preserve=ownership "lib" "readme.md" "$out/lib/nzbhydra2"
+    cp -dpr --no-preserve=ownership "readme.md" "$out/lib/nzbhydra2"
     install -D -m 755 "nzbhydra2wrapperPy3.py" "$out/lib/nzbhydra2/nzbhydra2wrapperPy3.py"
 
     makeWrapper ${python3}/bin/python $out/bin/nzbhydra2 \
@@ -37,7 +37,7 @@ stdenv.mkDerivation rec {
     description = "Usenet meta search";
     homepage = "https://github.com/theotherp/nzbhydra2";
     license = licenses.asl20;
-    maintainers = with maintainers; [jamiemagee];
+    maintainers = with maintainers; [ jamiemagee ];
     platforms = with platforms; linux;
     mainProgram = "nzbhydra2";
   };
