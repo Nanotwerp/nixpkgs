95855bc000bdeddfb7787f4dedaf0c11b2617617
Nano Twerpus <nanotwerp@gmail.com>
2024-05-31 11:28:59 -0400

    hmm

--- pkgs/desktops/gnome/extensions/defaultExtensions.nix
+++ pkgs/desktops/gnome/extensions/defaultExtensions.nix
@@ -1,6 +1,6 @@
 { callPackage }:
 {
   "system-monitor@gnome-shell-extensions.gcampax.github.com" =
-    callPackage ../core/gnome-shell-extensions { }
-      /egg;
+    callPackage ../core/gnome-shell-extensions
+      { };
 }
