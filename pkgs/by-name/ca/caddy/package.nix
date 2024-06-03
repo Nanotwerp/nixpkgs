fce0992252f16d4c2a675a7f494f8cb3dd00f21c
Nano Twerpus <nanotwerp@gmail.com>
2024-06-03 17:24:11 -0400

    caddy: allow adding external plugins Solves: #14671
    Caddy has support for plugins that are added at compile time. This exposes an argument `externalPlugins` that will build caddy with the specified plugins.
    
       Example:
       ```
       caddy-custom = pkgs.caddy.override {
         externalPlugins = [
           {name = "coraza"; repo = "github.com/corazawaf/coraza-caddy/v2"; version = "v2.0.0-rc.3";}
           {name = "cache-handler"; repo = "github.com/caddyserver/cache-handler"; version = "v0.9.0";}
           {name = "ratelimit"; repo = "github.com/mholt/caddy-ratelimit"; version = "2dc0d586f0b87e983757c403bc0929ddeb84a537";}
         ];
         vendorHash = "<SRI hash>";
       };
       ```
    
    Adapted from Brendan Taylor's CoreDNS change in commit 95e66809debf42dbe1e4935fd31c8c275914d2eb.

--- pkgs/by-name/ca/caddy/package.nix
+++ pkgs/by-name/ca/caddy/package.nix
@@ -1,13 +1,19 @@
-{ lib
-, buildGoModule
-, fetchFromGitHub
-, nixosTests
-, caddy
-, testers
-, installShellFiles
-, stdenv
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  gnused,
+  nixosTests,
+  caddy,
+  testers,
+  installShellFiles,
+  stdenv,
+  externalPlugins ? [ ],
+  vendorHash ? "sha256-1Api8bBZJ1/oYk4ZGIiwWCSraLzK9L+hsKXkFtk6iVM=",
 }:
 let
+  attrsToModules = map (plugin: plugin.repo);
+  attrsToSources = map ({ repo, version, ... }: lib.escapeShellArg "${repo}@${version}");
   version = "2.8.4";
   dist = fetchFromGitHub {
     owner = "caddyserver";
@@ -27,45 +33,79 @@ buildGoModule {
     hash = "sha256-CBfyqtWp3gYsYwaIxbfXO3AYaBiM7LutLC7uZgYXfkQ=";
   };
 
-  vendorHash = "sha256-1Api8bBZJ1/oYk4ZGIiwWCSraLzK9L+hsKXkFtk6iVM=";
+  inherit vendorHash;
 
   subPackages = [ "cmd/caddy" ];
 
   ldflags = [
-    "-s" "-w"
+    "-s"
+    "-w"
     "-X github.com/caddyserver/caddy/v2.CustomVersion=${version}"
   ];
 
   # matches upstream since v2.8.0
   tags = [ "nobadger" ];
 
-  nativeBuildInputs = [ installShellFiles ];
+  nativeBuildInputs = [
+    gnused
+    installShellFiles
+  ];
 
-  postInstall = ''
-    install -Dm644 ${dist}/init/caddy.service ${dist}/init/caddy-api.service -t $out/lib/systemd/system
+  postInstall =
+    ''
+      install -Dm644 ${dist}/init/caddy.service ${dist}/init/caddy-api.service -t $out/lib/systemd/system
 
-    substituteInPlace $out/lib/systemd/system/caddy.service \
-      --replace-fail "/usr/bin/caddy" "$out/bin/caddy"
-    substituteInPlace $out/lib/systemd/system/caddy-api.service \
-      --replace-fail "/usr/bin/caddy" "$out/bin/caddy"
-  '' + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-    # Generating man pages and completions fail on cross-compilation
-    # https://github.com/NixOS/nixpkgs/issues/308283
+      substituteInPlace $out/lib/systemd/system/caddy.service \
+        --replace-fail "/usr/bin/caddy" "$out/bin/caddy"
+      substituteInPlace $out/lib/systemd/system/caddy-api.service \
+        --replace-fail "/usr/bin/caddy" "$out/bin/caddy"
+    ''
+    + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+      # Generating man pages and completions fail on cross-compilation
+      # https://github.com/NixOS/nixpkgs/issues/308283
 
-    $out/bin/caddy manpage --directory manpages
-    installManPage manpages/*
+      $out/bin/caddy manpage --directory manpages
+      installManPage manpages/*
 
-    installShellCompletion --cmd caddy \
-      --bash <($out/bin/caddy completion bash) \
-      --fish <($out/bin/caddy completion fish) \
-      --zsh <($out/bin/caddy completion zsh)
-  '';
+      installShellCompletion --cmd caddy \
+        --bash <($out/bin/caddy completion bash) \
+        --fish <($out/bin/caddy completion fish) \
+        --zsh <($out/bin/caddy completion zsh)
+    '';
 
-  passthru.tests = {
-    inherit (nixosTests) caddy;
-    version = testers.testVersion {
-      command = "${caddy}/bin/caddy version";
-      package = caddy;
+  passthru = {
+    tests = {
+      inherit (nixosTests) caddy;
+      version = testers.testVersion {
+        command = "${caddy}/bin/caddy version";
+        package = caddy;
+      };
+    };
+    with-external-plugins = {
+      modBuildPhase = ''
+        for module in ${toString (attrsToModules externalPlugins)}; do
+          sed -i "/standard/a _ \"$module\"" ./cmd/caddy/main.go
+        done
+        for plugin in ${toString (attrsToSources externalPlugins)}; do
+          go get $plugin
+        done
+        go generate
+        go mod vendor
+      '';
+
+      modInstallPhase = ''
+        mv -t vendor go.mod go.sum
+        cp -r vendor "$out"
+      '';
+
+      preBuild = ''
+        chmod -R u+w vendor
+        [ -f vendor/go.mod ] && mv -t . vendor/go.{mod,sum}
+        go generate
+        for module in ${toString (attrsToModules externalPlugins)}; do
+          sed -i "/standard/a _ \"$module\"" ./cmd/caddy/main.go
+        done
+      '';
     };
   };
 
@@ -74,6 +114,10 @@ buildGoModule {
     description = "Fast and extensible multi-platform HTTP/1-2-3 web server with automatic HTTPS";
     license = licenses.asl20;
     mainProgram = "caddy";
-    maintainers = with maintainers; [ Br1ght0ne emilylange techknowlogick ];
+    maintainers = with maintainers; [
+      Br1ght0ne
+      emilylange
+      techknowlogick
+    ];
   };
 }
